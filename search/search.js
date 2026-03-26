9d18767b71a0ec598b205501e73e5e99e3d77a61
Jonathan <jonathan.mccullagh@skola.taby.se>
2026-03-26 13:40:03 +0100

    Fixat med lokala SVG och fixat lite mer med search.

--- search/search.js
+++ search/search.js
@@ -8,22 +8,37 @@ async function apiData() {
 
     for (let i = 0; i < api.products.length; i++) {
         const div = document.createElement("div");
-        div.classList.add("productPage")
-        div.id = api.products[i].id
+        div.classList.add("productPage");
+        div.id = api.products[i].id;
+        let productimage = "No_Image_Available.jpg";
+        if (api.products[i].imageLink !== "/images/") {
+            productimage = `https://raw.githubusercontent.com/jonmcc08/jonmcc08.github.io/main/fishingAPI${api.products[i].imageLink}`;
+        };
         div.innerHTML = `
         <a href="../products/?id=${api.products[i].id}">
-            <img class="productImage" src="https://raw.githubusercontent.com/jonmcc08/jonmcc08.github.io/main/fishingAPI${api.products[i].imageLink}">
+            <img class="productImage" src="${productimage}">
             <h3 class="productName pLeft">${api.products[i].name}</h3>
             <div class="productInfo pLeft">
                 <p class="productDescription"><i class="fa-solid fa-table-list"></i>${api.products[i].productDescription}</p>
                 <p class="productPrice">${api.products[i].price} kr</p>
             </div>
         </a>
-        ` // FUNGERAR
-        products.appendChild(div)
+        `; // FUNGERAR
+        products.appendChild(div);
     }
 }
 
 apiData(); // W.I.P (Testar att anropa "API"n)
 
 // FIXA SÅ ATT DEN OCKSÅ ANVÄNDER BILDER FRÅN GITHUB PAGEN OCH ÄVEN FIXA MED CSS DECORATION FÖR PRODUCTERNA
+
+const btn = document.getElementById("searchBtn");
+const searchValue = document.getElementById("search");
+
+btn.addEventListener("click", function (e) {
+    if (searchValue.value != "") {
+        window.location.href = `?search=${searchValue.value}`
+    } else {
+        window.location.href = ""
+    }
+})
