c00a4e09535b469778e944675fe5d2d3ca14ac01
Jonathan <jonathan.mccullagh@skola.taby.se>
2026-04-23 13:38:23 +0200

    Fixar med sökfunktion, TODO: Fixa med typ av produkt

--- search/search.js
+++ search/search.js
@@ -1,52 +1,89 @@
+const btn = document.getElementById("searchBtn");
+const searchValue = document.getElementById("search");
+const searchParams = new URLSearchParams(window.location.search);
 
-
-async function apiData() {
+async function apiData(search, searchTerm) {
 
     const response = await fetch("https://raw.githubusercontent.com/jonmcc08/jonmcc08.github.io/main/fishingAPI/products.json");
     const api = await response.json();
     console.log(api);
 
-    const products = document.getElementById("products");
+    const productsPage = document.getElementById("products");
+    
+    let productList = api.products
 
-    for (let i = 0; i < api.products.length; i++) {
+    if (search) {
+        productList = searchListing(searchTerm.split(" "), productList)
+    }
+
+    for (let i = 0; i < productList.length; i++) {
         const div = document.createElement("div");
         div.classList.add("productPage");
-        div.id = api.products[i].id;
+        div.id = productList[i].id
         let productimage = "../No_Image_Available.jpg";
-        if (api.products[i].imageLink !== "/images/") {
-            productimage = `https://raw.githubusercontent.com/jonmcc08/jonmcc08.github.io/main/fishingAPI${api.products[i].imageLink}`;
+        if (productList[i].imageLink !== "/images/") {
+            productimage = `https://raw.githubusercontent.com/jonmcc08/jonmcc08.github.io/main/fishingAPI${productList[i].imageLink}`;
         };
         div.innerHTML = `
-        <a href="../products/index.html?id=${api.products[i].id}">
+        <a href="../products/index.html?id=${productList[i].id}">
             <img class="productImage" src="${productimage}">
-            <h3 class="productName pLeft">${api.products[i].name}</h3>
+            <h3 class="productName pLeft">${productList[i].name}</h3>
             <div class="productInfo pLeft">
-                <p class="productDescription"><i class="fa-solid fa-table-list"></i>${api.products[i].productDescription}</p>
-                <p class="productPrice">${api.products[i].price} kr</p>
+                <p class="productDescription"><i class="fa-solid fa-table-list"></i>${productList[i].productDescription}</p>
+                <p class="productPrice">${productList[i].price} kr</p>
             </div>
         </a>
         `; // FUNGERAR
-        products.appendChild(div);
+        productsPage.appendChild(div);
     }
 }
 
-apiData();
-const btn = document.getElementById("searchBtn");
-const searchValue = document.getElementById("search");
-
-btn.addEventListener("click", function (e) {
-    if (searchValue.value != "") {
-        window.location.href = `?search=${searchValue.value}`
-    } else {
-        window.location.href = ""
+function searchPress(e) {
+    
+    const isClicked = e.type === "click"
+    const isEnterPressed = e.key === "Enter"
+    if (isClicked || isEnterPressed) {
+        if (searchValue.value != "") {
+        searchParams.set("search", searchValue.value)
+        } else {
+            searchParams.delete("search")
+        }
+        window.location.search = searchParams.toString();
     }
-})
+    console.log("Works.")
+}
 
-const searchParams = new URLSearchParams(window.location.search)
+function searchListing(searchTerm, products) {
+    // Testing
+    console.log(products)
+    console.log(searchTerm)
+
+    let newProducts = []
+
+    searchTerm.forEach(sentance => {
+        console.log("Current checking: " + sentance)
+        for(let i = 0; i < products.length; i++) {
+            const productName = products[i].name.toLowerCase()
+            console.log("Does it contain: " + productName)
+            if(productName.includes(sentance)) {
+                newProducts.push(products[i])
+            }
+    }})
+
+    return newProducts
+}
+
+btn.addEventListener("click", searchPress)
+searchValue.addEventListener("keydown", searchPress)
+
 
 if (searchParams.size === 0) {
-    console.log("Test")
+    apiData();
 } else {
-    console.log(searchParams)
+    const searchTerm = searchParams.get("search")
+    console.log(searchTerm)
+    searchValue.value = searchTerm
+
+    apiData(true, searchTerm.toLowerCase());
 }
  // W.I.P (Testar att anropa "API"n)
